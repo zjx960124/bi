f4bf8bf8b862cd7752abf31eb32a6974867c04a1
599081799@qq.com <599081799@qq.com>
2022-11-03 16:44:57 +0800

    提交

--- src/views/construction/datasource/index.vue
+++ src/views/construction/datasource/index.vue
@@ -4,22 +4,11 @@
       <div class="datasource-container">
         <el-row>
           <el-col :span="20">
-            <div class="datasource-content">
-              <el-empty :image="imgUrl" />
-              <div class="datasource-action">
-                <div class="datasource-action-title">获取数据</div>
-                <div class="datasource-action-content">
-                  <div class="item">
-                    <div class="item-img"></div>
-                    <div class="item-title">MySQL</div>
-                  </div>
-                  <div class="item"></div>
-                  <div class="item"></div>
-                </div>
-              </div>
-            </div>
+            <list />
           </el-col>
-          <el-col :span="4"></el-col>
+          <el-col :span="4">
+            <setting />
+          </el-col>
         </el-row>
       </div>
     </template>
@@ -27,72 +16,16 @@
 </template>
 
 <script lang="ts" setup>
-import noData from "@/assets/data/noData.png";
-import { ref } from "vue";
-const imgUrl = ref(noData);
+import list from "./components/list.vue";
+import setting from "./components/setting.vue";
 </script>
 <style scoped lang="scss">
 .datasource-container {
   width: 100%;
-  height: calc(100vh - 170px);
+  height: calc(100vh - 180px);
   & .el-row {
     width: 100%;
     height: 100%;
-  }
-  .datasource-content {
-    width: 100%;
-    height: 100%;
-    background: #fff;
-    border-radius: 25px;
-    position: relative;
-    .datasource-action {
-      width: 100%;
-      height: 280px;
-      position: absolute;
-      bottom: 30px;
-      .datasource-action-title {
-        font-size: 14px;
-        font-family: "PingFang SC";
-        font-weight: 500;
-        color: #293270;
-        line-height: 52px;
-        text-align: left;
-        padding-left: 45px;
-      }
-      .datasource-action-content {
-        display: flex;
-        height: calc(100% - 52px);
-        padding: 0 27px;
-        box-sizing: border-box;
-        & .item {
-          flex: 0.3;
-          height: 100%;
-          background: #f3f5ff;
-          border-radius: 15px;
-          margin-right: 15px;
-          cursor: pointer;
-          &:last-child {
-            margin-right: 0;
-          }
-          .item-img {
-            height: calc(100% - 42px);
-            border-top-left-radius: 15px;
-            border-top-right-radius: 15px;
-            background-color: #bfbfbf;
-          }
-          .item-title {
-            font-size: 14px;
-            font-family: "PingFang SC";
-            font-weight: 400;
-            color: #293270;
-            text-align: left;
-            line-height: 42px;
-            padding-left: 15px;
-            box-sizing: border-box;
-          }
-        }
-      }
-    }
   }
 }
 </style>

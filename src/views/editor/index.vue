3a18698f61a4fcf0095e7048d1f18515d305134c
jx <18965183850@163.com>
2022-11-03 15:30:23 +0800

    页面模块，元素模块提交

--- src/views/editor/index.vue
+++ src/views/editor/index.vue
@@ -5,15 +5,18 @@ import { useContextMenu } from "./charts/hooks/useContextMenu.hook";
 import { useChartEditStore } from "@/store/chartEditStore/chartEditStore";
 import { useChartHistoryStore } from "@/store/chartHistoryStore/chartHistoryStore";
 import { useRouter } from "vue-router";
+import { ChevronBack, ArrowUndo, ArrowRedo } from "@vicons/ionicons5";
+import { reactive, ref, computed } from "vue";
+import { icon } from "@/plugins";
 
 const charts = loadAsyncComponent(() => import("./charts/index.vue"));
 const pages = loadAsyncComponent(() => import("./pages/index.vue"));
 const operation = loadAsyncComponent(() => import("./operation/index.vue"));
 
-const chartHistoryStoreStore = useChartHistoryStore();
+const chartHistoryStore = useChartHistoryStore();
 const chartEditStore = useChartEditStore();
 // 记录初始化
-chartHistoryStoreStore.canvasInit(chartEditStore.getEditCanvas);
+chartHistoryStore.canvasInit(chartEditStore.getEditCanvas);
 
 // 路由
 const router = useRouter();
@@ -22,20 +25,85 @@ const currentRoute = router.currentRoute.value.name;
 // 右键
 const { menuOptions, onClickOutSide, mousePosition, handleMenuSelect } =
   useContextMenu();
+
+// 画板尺寸
+const sizeValue = ref<number>(0);
+const sizeOptions = reactive([
+  {
+    label: "1920*1080（默认）",
+    value: 0,
+  },
+]);
+
+// 前进后退按钮
+const isBackStack = computed(() => chartHistoryStore.getBackStack.length > 1);
+const isForwardStack = computed(
+  () => chartHistoryStore.getForwardStack.length > 0
+);
+const historyList = reactive([
+  {
+    key: "backStack",
+    select: isBackStack,
+    title: "后退",
+    icon: ArrowUndo,
+  },
+  {
+    key: "forward",
+    select: isForwardStack,
+    title: "前进",
+    icon: ArrowRedo,
+  },
+]);
+// 历史记录处理
+const clickHistoryHandle = (key: string) => {
+  switch (key) {
+    case "backStack":
+      chartEditStore.setBack();
+      break;
+    case "forward":
+      chartEditStore.setForward();
+      break;
+  }
+};
 </script>
 <template>
   <div class="editor">
     <header-plugin>
       <template #left>
-        <span>大屏名称</span>
-        <el-button>撤销</el-button>
-        <el-button>下一步</el-button>
+        <n-button round color="#6D79FF" class="return-btn">
+          <template #icon>
+            <n-icon :component="ChevronBack"></n-icon>
+          </template>
+          返回
+        </n-button>
       </template>
-      <template #center></template>
+      <template #center>
+        <div class="screen-name">未命名可视化大屏</div>
+        <div class="size">
+          <span>画板尺寸</span>
+          <n-space vertical>
+            <n-select
+              v-model:value="sizeValue"
+              size="small"
+              :options="sizeOptions"
+            />
+          </n-space>
+        </div>
+      </template>
       <template #ri-left>
-        <el-button>预览</el-button>
-        <el-button>保存</el-button>
-        <el-button>保存并发布</el-button>
+        <n-button
+          v-for="item in historyList"
+          :key="item.key"
+          text
+          :disabled="!item.select"
+          @click="clickHistoryHandle(item.key)"
+        >
+          <n-icon
+            :component="item.icon"
+            size="24"
+            style="margin-right: 29px"
+          ></n-icon>
+        </n-button>
       </template>
     </header-plugin>
     <main>
@@ -62,10 +130,31 @@ const { menuOptions, onClickOutSide, mousePosition, handleMenuSelect } =
   width: 100vw;
   height: 100vh;
   overflow: hidden;
+  background: #f3f5ff;
+  .screen-name {
+    font-size: 24px;
+    font-family: "PingFang SC";
+    font-weight: bold;
+    color: #293270;
+    margin-left: 44px;
+  }
+  .size {
+    display: flex;
+    align-items: center;
+    margin-right: 132px;
+    & > span {
+      color: #6b797f;
+      font-size: 14px;
+      font-family: "PingFang SC";
+      margin-right: 9px;
+    }
+  }
   main {
     overflow: hidden;
     display: flex;
     height: calc(100% - 60px);
+    padding-bottom: 13px;
+    box-sizing: border-box;
   }
 }
 </style>
--- src/views/editor/operation/editorToolbar/index.vue
+++ src/views/editor/operation/editorToolbar/index.vue
@@ -13,27 +13,46 @@ const chartsItem = loadAsyncComponent(
 </script>
 <template>
   <div class="editor-toolbar">
-    <template v-for="(item, index) in genreMenuOptions" :key="index">
-      <el-dropdown trigger="click">
-        <el-button style="margin-right: 20px">{{ item.label }}</el-button>
-        <template #dropdown>
-          <template v-for="(items, indexs) in item.list" :key="indexs">
-            <div>
-              <el-button type="text">{{ items.label }}</el-button>
-            </div>
+    <div class="editor-toolbar-view">
+      <div class="magnify"></div>
+      <template v-for="(item, index) in genreMenuOptions" :key="index">
+        <el-dropdown trigger="click">
+          <n-button quaternary type="primary" color="#8386A1">
+            {{ item.label }}
+            <template #icon>
+              <n-icon>
+                <cash-icon />
+              </n-icon>
+            </template>
+          </n-button>
+          <template #dropdown>
+            <template v-for="(items, indexs) in item.list" :key="indexs">
+              <div>
+                <el-button type="text">{{ items.label }}</el-button>
+              </div>
+            </template>
+            <charts-item :menuOptions="item.all"></charts-item>
           </template>
-          <charts-item :menuOptions="item.all"></charts-item>
-        </template>
-      </el-dropdown>
-    </template>
+        </el-dropdown>
+      </template>
+    </div>
   </div>
 </template>
 <style lang='scss' scoped>
 .editor-toolbar {
-  width: calc(100% - 350px);
-  height: 55px;
+  width: calc(100% - 469px);
+  height: 67px;
   position: absolute;
   left: 0;
   top: 0;
+  background-color: #f3f5ff;
+  .editor-toolbar-view {
+    background: #ffffff;
+    margin-bottom: 17px;
+    border-radius: 15px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+  }
 }
 </style>
